c5ae197a755a07a332074f4e64c3e0a869c355d2
Omar Abdelazim <omarabdelazem51@gmail.com>
2026-09-21 22:21:42 +0300

    Delete CatalogItem.java

--- CatalogItem.java
+++
@@ -1,5 +0,0 @@
-package iti.iwish.shared;
-
-import java.io.Serializable;
-
-public record CatalogItem(long id, String name, String category, double price) implements Serializable { private static final long serialVersionUID = 1L; }
