0ed22a0656dddc3d77107ea313ca8fe4e64e1556
Omar Abdelazim <omarabdelazem51@gmail.com>
2026-09-21 22:20:59 +0300

    Delete ApiRequest.java

--- ApiRequest.java
+++
@@ -1,9 +0,0 @@
-package iti.iwish.shared;
-
-import java.io.Serializable;
-import java.util.Map;
-
-/** Small dependency-free request envelope for the desktop client/server protocol. */
-public record ApiRequest(String action, Map<String, Object> data) implements Serializable {
-    private static final long serialVersionUID = 1L;
-}
